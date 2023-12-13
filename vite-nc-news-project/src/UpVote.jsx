88abf18c275d6c28517e5d950bb7f8cfbf8ba829
imaa04 <imakhan.04@gmail.com>
2023-12-13 16:01:35 +0000

    disable vote button after voting

--- vite-nc-news-project/src/UpVote.jsx
+++ vite-nc-news-project/src/UpVote.jsx
@@ -4,10 +4,10 @@ import { useParams } from "react-router-dom";
 
 export default function UpVote({currVotes}){
     const [votes, setUpVote] = useState(currVotes)
+    const [isClicked, setIsClicked] = useState(false);
     const {article_id} = useParams()
     
     const handleVote = (voteChange) => {
-        
         setUpVote(votes + voteChange)
         
         patchVotes(article_id, voteChange)
@@ -15,19 +15,22 @@ export default function UpVote({currVotes}){
             console.log(`votes for article ${article_id} updated to`, res);
         })
         .catch((err) => {
-            console.log(`arror adding vote to article ${article_id}`, err);
+            console.log(`error adding vote to article ${article_id}`, err);
             setUpVote(votes)
         })
+        .finally(() => {
+            setIsClicked(true)
+        })
+        
     }
-    
     
 
     return (
         <>
-        <button onClick={()=> handleVote(1)}>
+            <button onClick={()=> handleVote(1)} disabled={isClicked}>
             UpVote +1
         </button>
-            <button onClick={()=> handleVote(-1)}>
+            <button onClick={() => handleVote(-1)} disabled={isClicked}>
                 DownVote -1
             </button>
         <p>UpVote: {votes}</p>
